51847058e2f2dc6cc92eb27b6d9b22a05583346e
Simon Brewer <brewersi@gmail.com>
2019-04-19 20:20:59 -0600

    Corrected path and file names

--- 02_runClam.R
+++ 02_runClam.R
@@ -10,13 +10,14 @@ for (i in 1:ncore) {
   print(paste("Doing",i,"of",ncore))
   ageFile = paste0(coreDir,coreList[i],"/",coreList[i],".csv")
   ages = read.csv(ageFile)
+  nages = nrow(ages)
   
   ## Run clam
   if (nages >= 2) {
-    clam(coreName, depths.file=TRUE, coredir = "./agemodels/clam/Cores/")
+    clam(coreList[i], depths.file=TRUE, coredir = "./agemodels/clam/Cores/")
     
-    if (nages >= 4 & epdEnts$E[i]!=2145) {
-      clam(coreName, depths.file=TRUE, type = 4, coredir = "./agemodels/clam/Cores/")
+    if (nages >= 4) {
+      clam(coreList[i], depths.file=TRUE, type = 4, coredir = "./agemodels/clam/Cores/")
       
     }
   }
